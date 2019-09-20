8dc8d5116211c5ce99c66ec2e095badbdfc4f41d
huybui <huythdnbkltv@gmail.com>
2019-09-20 13:33:11 +0300

    Add features: delete tasks

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+
+import { Tasks } from '../api/tasks.js';
  
 // Task component - represents a single todo item
 export default class Task extends Component {
+  handleDeleteClick = () => {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const taskClassName = this.props.task.checked ? 'Checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button 
+          className='delete'
+          onClick={this.handleDeleteClick}
+        >
+          &times;
+        </button>
+        <span className='text'>{this.props.task.text}</span>
+      </li>
     );
   }
 }

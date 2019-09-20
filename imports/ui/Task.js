c1fc810405087d716164dc7e115a9dea2fb6d4e0
huybui <huythdnbkltv@gmail.com>
2019-09-20 17:05:36 +0300

    Show username next to each task

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -36,7 +36,9 @@ export default class Task extends Component {
           onClick={this.handleCheckboxClick}
         />
 
-        <span className='text'>{this.props.task.text}</span>
+        <span className='text'>
+          <strong>{this.props.task.username}</strong>:  {this.props.task.text}
+        </span>
       </li>
     );
   }
